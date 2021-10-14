8fd669d884093b34ecc02882590cd3153ad4886c
Jaime <alu0101329093@ull.edu.es>
2021-10-14 17:46:16 +0100

    Added play button stop animation

--- renderer.js
+++ renderer.js
@@ -4,6 +4,7 @@ const menuButtonCar = document.getElementById('menu-button-car');
 const menuButtonObstacle = document.getElementById('menu-button-obstacle');
 const menuButtonGoal = document.getElementById('menu-button-goal');
 const menuButtonPath = document.getElementById('menu-button-path');
+const menuPlayButton = document.getElementById('menu-play-button');
 
 menuButtonCar.getElementsByClassName('menu-button-color')[0].style.
     backgroundColor = 'red';
@@ -14,10 +15,14 @@ menuButtonGoal.getElementsByClassName('menu-button-color')[0].style.
 menuButtonPath.getElementsByClassName('menu-button-color')[0].style.
     backgroundColor = 'blue';
 
+menuPlayButton.onclick = () => {
+  menuPlayButton.classList.toggle('pause');
+};
+
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas');
 const prueba =
-  new MatrixDisplay(canvas.getContext('2d'), 100, 50, 10, 10);
+  new MatrixDisplay(canvas.getContext('2d'), 100, 45, 10, 10);
 
 document.addEventListener('keyup', (key) => {
   if (key.code == 'Space') {
